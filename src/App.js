dbdbc3d6674a8bf50e27834a1c82ecd0b16236ac
bvibsIACUSBCIU <bitcoin24@163.com>
2025-07-29 14:45:23 +0700

    Initial commit for my static page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'; // 保留 CSS 引入，后面我们会简单修改一下
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>欢迎来到我的静态网页！</h1>
+        <p>这是我用 React 制作的第一个简单页面。1234567</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
